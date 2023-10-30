167ec23f42b421706507b24a78f47619753efbce
arvndblocker"nn <aravindvasang@gmail.com>
2023-10-30 13:44:03 +0530

    eth_bank

---
+++ public/app.js
@@ -0,0 +1,116 @@
+var ContractABI = [
+  {
+    inputs: [],
+    name: "deposite",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_amount",
+        type: "uint256",
+      },
+    ],
+    name: "withdraw",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_amount",
+        type: "uint256",
+      },
+    ],
+    name: "withdrawfunds",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getbalance",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getcontractbalance",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+var ContractAddress = "0xbC5b68Adb2386e3cBFd426D038B3Dd166e07debE";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  if (typeof window.ethereum !== "undefined") {
+    console.log("metamask is installed");
+
+    var accounts = await ethereum.request({ method: "eth_requestAccounts" });
+    console.log(accounts);
+
+    web3 = new Web3(window.ethereum);
+    console.log("web3 is loaded", web3);
+
+    myContract = new web3.eth.Contract(ContractABI, ContractAddress);
+    console.log("contract is loaded", myContract);
+  } else {
+    alert("please install metamask!");
+  }
+});
